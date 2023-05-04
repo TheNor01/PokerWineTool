eb3ebbabdcbe7fff823f92c8c6462731dd1148ab
Aldo Fiorito <aldo.fiorito97@gmail.com>
2023-05-04 22:27:53 +0200

    linear metrics

--- main_wine.py
+++ main_wine.py
@@ -35,9 +35,11 @@ import os
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sn
+import numpy as np
 from scipy.stats import zscore
 from sklearn.model_selection import train_test_split
-
+from sklearn.linear_model import LinearRegression,LogisticRegression,RidgeCV
+from sklearn.metrics import r2_score,mean_squared_error,mean_absolute_error
 
 rows = []
 
@@ -119,7 +121,10 @@ print(unionWine.describe())
 print("zSCORE")
 X_train_scored = pd.DataFrame()
 for col in unionWine.columns:
-        X_train_scored[str(col)] = zscore(unionWine[col])
+        if(col=="wineLabel"): 
+                X_train_scored[str(col)] = unionWine[col]
+        else:
+                X_train_scored[str(col)] = zscore(unionWine[col])
 
 
 print(X_train_scored)
@@ -127,7 +132,7 @@ print(X_train_scored.describe())
 
 #check covariance matrice
 #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.cov.html
-cov_matrix = pd.DataFrame.cov(dataWineRed)
+cov_matrix = pd.DataFrame.cov(unionWine)
 sn.heatmap(cov_matrix, annot=False, fmt='g')
 plt.show()
 
@@ -136,13 +141,13 @@ plt.show()
 fig, ax = plt.subplots(figsize=(10, 6))
 
 
-pearsonDf = dataWineRed.corr().abs()
+pearsonDf = unionWine.corr().abs()
 sn.heatmap(pearsonDf, ax=ax, annot=True)
 plt.title("pearson CORR")
 plt.show()
 
 
-kendallDf = dataWineRed.corr(method='kendall').abs()
+kendallDf = unionWine.corr(method='kendall').abs()
 fig, ax = plt.subplots(figsize=(10, 6))
 sn.heatmap(kendallDf, ax=ax, annot=True)
 plt.title("kendall CORR")
@@ -158,13 +163,43 @@ print(so[(so >= 0.50) & (so < 1.00)])
 print("-----------")
 print(so1[(so1 >= 0.50) & (so1 < 1.00)])
 
+colors = ['#E69F00', '#56B4E9']
+names= ['red','white']
+#plot different chart for two label
+#for col in unionWine.columns:
+
+
+"""
+for s in unionWine.columns:
+# plotting both distibutions on the same figure
+        sn.scatterplot(data=unionWine, x='pH',y=s, hue='wineLabel',alpha=0.5)
+        plt.show()
+"""
 
 y_train_scored = X_train_scored.loc[:, X_train_scored.columns == 'wineLabel'].values.ravel()
 
 X_train, X_test, y_train, y_test = train_test_split(X_train_scored, y_train_scored, test_size = 0.2, random_state = 42)
+
+
+X_test.drop('wineLabel', axis=1, inplace=True)
+X_train.drop('wineLabel', axis=1, inplace=True)
+
+
+print(X_test)
+print(y_test)
 """
-for col in dataWineRed.columns:
-        dataWineRed[col].plot(kind='kde')
-        plt.title(str(col))
-        plt.show()
+REGRESSION LINEAR
+
+
 """
+
+regressorLinear = LinearRegression()
+regressorLinear.fit(X_train,y_train)
+
+y_predective_linear = regressorLinear.predict(X_test)
+score = r2_score(y_test,y_predective_linear)*100
+print("R2 Score original",score)
+
+print('MAE:', mean_absolute_error(y_test, y_predective_linear))
+print('MSE:', mean_squared_error(y_test, y_predective_linear))
+print('RMSE:', np.sqrt(mean_squared_error(y_test, y_predective_linear)))
